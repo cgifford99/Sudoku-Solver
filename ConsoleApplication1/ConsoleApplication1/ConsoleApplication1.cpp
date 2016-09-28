2fdace5488f5a65e8ff2bf6d53c88c5b23eb7820
cgifford99 <cmgskiguy99@gmail.com>
2016-09-28 12:39:08 -0400

    Version 0.0.2
    Added difficulty selection. Trying to create a dynamically sized array.
    Crashes after selection.

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,56 +10,71 @@ class Board {
 public:
 	int sizeX;
 	int sizeY;
+	int** sBoard;
 	const enum diffInput { easy, medium, hard, };
 
-	Board() {
-		int din;
+	Board(){
 		cout << "Enter difficulty: Easy, Medium or Hard?";
 		cin >> din;
 		switch (din) {
 		case easy:
-			sizeX = 9;
-			sizeY = sizeX;
+			sizeX = 6;
+			sizeY = 6;
 			break;
 		case medium:
 			sizeX = 9;
-			sizeY = sizeX;
+			sizeY = 9;
 			break;
 		case hard:
-			sizeX = 9;
-			sizeY = sizeX;
+			sizeX = 16;
+			sizeY = 16;
 			break;
 		}
+		allocate(sBoard);
+		sBoard[sizeX][sizeY];
 	}
+	
+	void allocate(int** arr) {
+		arr = new int*[sizeX];
+		for (int x = 0; x < sizeX; x++) {
+			arr[x] = new int[sizeY];
+		}
+		cout << sizeX << endl;
+		cout << sizeY << endl;
+	}
+
+protected:
+	int din;
 };
 
 class Sudoku : public Board {
 public:
-	int board[9][9]{
-	};
-	void createBoard(int arr[9][9]) {
-		for (int d = 0; d < 9; d++) {
-			for (int e = 0; e < 9; e++) {
-				arr[d][e] = 1 + (rand() % 9);
-
+	void createBoard(int** arr) {
+		arr[sizeX][sizeY];
+		for (int d = 0; d < sizeX; d++) {
+			for (int e = 0; e < sizeY; e++) {
+				arr[d][e] = 1 + (rand() % sizeX);
 			}
 		}
 		for (int r = 0; r < 35; r++) {
-			arr[(0 + (rand() % 8))][(0 + (rand() % 8))] = 0;
+			arr[(0 + (rand() % sizeX-1))][(0 + (rand() % sizeX-1))] = 0;
 		}
 	}
 
-	void printBoard(int arr[9][9]) {
-		for (int b = 0; b < 9; b++) {
+	void printBoard(int** arr) {
+		arr[sizeX][sizeY];
+		for (int b = 0; b < sizeX; b++) {
+			/*
 			if (b == 3 || b == 6 || b == 9) {
 				cout << "| ------+-------+--------" << endl;
 			}
 			cout << "| ";
+			*/
 			for (int c = 0; c < 9; c++) {
 				cout << arr[b][c] << " ";
-				if (c == 2 || c == 5 || c == 8) {
-					cout << "| ";
-				}
+				//if (c == 2 || c == 5 || c == 8) {
+				//	cout << "| ";
+				//}
 			}
 			cout << endl;
 		}
@@ -67,19 +82,8 @@ public:
 };
 
 int main() {
-	Board setUp;
-	//Sudoku gameObj;
-	
-	//gameObj.createBoard(gameObj.board);
-	//gameObj.printBoard(gameObj.board);
-
-	
-	/*
-	int array1[9]{ 0,1,2,3,4,5,6,7,8 };
-	for (int i = 0; i < (sizeof(array1) / sizeof(*array1)); i++) {
-		cout << array1[i] << endl;
-	}
-	*/
-
+	Sudoku gameObj; 
+	gameObj.createBoard(gameObj.sBoard);
+	gameObj.printBoard(gameObj.sBoard);
 	return 0;
 }
