ff8a6f26fe287ecfa5f6f1f3f7ba68b0b16d9cb9
Christopher Gifford <cmgskiguy99@gmail.com>
2016-09-27 20:48:38 -0400

    Version 0.0.1a
    Starting to program the board set-up. Tried overloading the  '>>'
    operator to work with enums, but never got it going. Ended up doing a
    much simpler method. (All in the Board class)

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,35 +2,56 @@
 //
 
 #include "stdafx.h"
+#include <string>
 #include <iostream>
 using namespace std;
 
-class Sudoku {
-public: 
+class Board {
+public:
+	int sizeX;
+	int sizeY;
+	const enum diffInput { easy, medium, hard, };
+
+	Board() {
+		int din;
+		cout << "Enter difficulty: Easy, Medium or Hard?";
+		cin >> din;
+		switch (din) {
+		case easy:
+			sizeX = 9;
+			sizeY = sizeX;
+			break;
+		case medium:
+			sizeX = 9;
+			sizeY = sizeX;
+			break;
+		case hard:
+			sizeX = 9;
+			sizeY = sizeX;
+			break;
+		}
+	}
+};
+
+class Sudoku : public Board {
+public:
 	int board[9][9]{
-		1,2,3   ,4,5,6    ,7,8,9,
-		4,5,6   ,7,8,9    ,1,2,3,
-		7,8,9   ,1,2,3    ,4,5,6,
-
-		0,0,0   ,7,0,5    ,0,0,8,
-		0,0,0   ,0,0,0    ,0,0,0,
-		4,0,0   ,2,0,8    ,0,0,0,
-
-		7,0,4   ,0,0,3    ,0,0,0,
-		0,5,2   ,0,0,1    ,0,0,9,
-		1,0,8   ,0,0,0    ,6,0,0
 	};
 	void createBoard(int arr[9][9]) {
 		for (int d = 0; d < 9; d++) {
 			for (int e = 0; e < 9; e++) {
 				arr[d][e] = 1 + (rand() % 9);
+
 			}
+		}
+		for (int r = 0; r < 35; r++) {
+			arr[(0 + (rand() % 8))][(0 + (rand() % 8))] = 0;
 		}
 	}
 
 	void printBoard(int arr[9][9]) {
 		for (int b = 0; b < 9; b++) {
-			if (b == 3 || b == 6 || b == 9){
+			if (b == 3 || b == 6 || b == 9) {
 				cout << "| ------+-------+--------" << endl;
 			}
 			cout << "| ";
@@ -38,7 +59,7 @@ public:
 				cout << arr[b][c] << " ";
 				if (c == 2 || c == 5 || c == 8) {
 					cout << "| ";
-				} //Github tests
+				}
 			}
 			cout << endl;
 		}
@@ -46,10 +67,13 @@ public:
 };
 
 int main() {
-	Sudoku gameObj;
-	gameObj.createBoard(gameObj.board);
-	gameObj.printBoard(gameObj.board);
+	Board setUp;
+	//Sudoku gameObj;
+	
+	//gameObj.createBoard(gameObj.board);
+	//gameObj.printBoard(gameObj.board);
 
+	
 	/*
 	int array1[9]{ 0,1,2,3,4,5,6,7,8 };
 	for (int i = 0; i < (sizeof(array1) / sizeof(*array1)); i++) {
