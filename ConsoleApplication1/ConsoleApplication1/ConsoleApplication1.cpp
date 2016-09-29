4d3b052a558bd2947e0a09aca8ba1ef2ff925a3e
cgifford99 <cmgskiguy99@gmail.com>
2016-09-29 13:20:20 -0400

    Version 0.0.3
    Removed dynamically sized array. Proved unnecessary at the moment.
    Slowing producing a random sudoku generator (all randomized board will be
    solved for now)

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,86 +4,72 @@
 #include "stdafx.h"
 #include <string>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 class Board {
 public:
 	int sizeX;
 	int sizeY;
-	int** sBoard;
+	int sBoard[9][9];
 	const enum diffInput { easy, medium, hard, };
-
+	
 	Board(){
+		/*
 		cout << "Enter difficulty: Easy, Medium or Hard?";
 		cin >> din;
 		switch (din) {
 		case easy:
-			sizeX = 6;
-			sizeY = 6;
+			blanks = 36;
 			break;
 		case medium:
-			sizeX = 9;
-			sizeY = 9;
+			blanks = 45;
 			break;
 		case hard:
-			sizeX = 16;
-			sizeY = 16;
-			break;
-		}
-		allocate(sBoard);
-		sBoard[sizeX][sizeY];
+			blanks = 54;
+		*/
 	}
 	
-	void allocate(int** arr) {
-		arr = new int*[sizeX];
-		for (int x = 0; x < sizeX; x++) {
-			arr[x] = new int[sizeY];
+	void genBoard() {
+		for (int x = 0; x < 9; x++) {
+			for (int y = 0; y < 9; y++) {
+				sBoard[x][y] = (1 + (rand() % 9));
+				var = find(sBoard.first, sBoard.last, 1);
+			}
 		}
-		cout << sizeX << endl;
-		cout << sizeY << endl;
 	}
 
 protected:
 	int din;
+	int blanks;
+	int var;
 };
 
 class Sudoku : public Board {
 public:
-	void createBoard(int** arr) {
-		arr[sizeX][sizeY];
-		for (int d = 0; d < sizeX; d++) {
-			for (int e = 0; e < sizeY; e++) {
-				arr[d][e] = 1 + (rand() % sizeX);
-			}
-		}
-		for (int r = 0; r < 35; r++) {
-			arr[(0 + (rand() % sizeX-1))][(0 + (rand() % sizeX-1))] = 0;
-		}
-	}
 
-	void printBoard(int** arr) {
-		arr[sizeX][sizeY];
-		for (int b = 0; b < sizeX; b++) {
-			/*
+	void printBoard(int arr[9][9]) {
+		cout << " ----------------------- " << endl;
+		for (int b = 0; b < 9; b++) {
 			if (b == 3 || b == 6 || b == 9) {
 				cout << "| ------+-------+--------" << endl;
 			}
 			cout << "| ";
-			*/
 			for (int c = 0; c < 9; c++) {
 				cout << arr[b][c] << " ";
-				//if (c == 2 || c == 5 || c == 8) {
-				//	cout << "| ";
-				//}
+				if (c == 2 || c == 5 || c == 8) {
+					cout << "| ";
+				}
 			}
 			cout << endl;
 		}
+		cout << " ----------------------- " << endl;
 	}
 };
 
 int main() {
 	Sudoku gameObj; 
-	gameObj.createBoard(gameObj.sBoard);
+	gameObj.genBoard();
 	gameObj.printBoard(gameObj.sBoard);
 	return 0;
 }
