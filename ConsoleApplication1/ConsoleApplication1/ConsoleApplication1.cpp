5308c8cbb1ae0a9e6a93a05657c1251d2ff39484
Christopher Gifford <cmgskiguy99@gmail.com>
2016-10-05 23:28:53 -0400

    Version 0.0.5a
    Not much has changed other than some debug tests and comments. Also
    figured out the vector.erase function doesn't work; doesn't do anything
    with no errors

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,12 +10,15 @@ using namespace std;
 
 class Board {
 public:
-	int sizeX;
-	int sizeY;
+	//int sizeX;
+	//int sizeY;
 	int sBoard[9][9];
-	const enum diffInput { easy, medium, hard, };
+	//const enum diffInput { easy, medium, hard, };
 	
 	Board(){
+		for (int i = 0; i <= 8; i++) { //Adds values to the vector
+			numVec.push_back(i);
+		}
 		/* Temp comment. Difficulty option currently unnecessary.
 		cout << "Enter difficulty: Easy, Medium or Hard?";
 		cin >> diffIn;
@@ -32,10 +35,26 @@ public:
 	}
 	
 	void genBoard() {
-		for (int i = 1; i <= 9; i++) { //Adds values to the vector
-			numVec.push_back(i);
-		}
 		for (int x = 0; x < 9; x++) { //Sets values for the board
+
+			/************************TEMP DEBUG CODE*****************************/
+			cout << " ----------------------- " << endl;
+			for (int b = 0; b < 9; b++) {
+				if (b == 3 || b == 6 || b == 9) {
+					cout << "| ------+-------+--------" << endl;
+				}
+				cout << "| ";
+				for (int c = 0; c < 9; c++) {
+					cout << sBoard[b][c] << " ";
+					if (c == 2 || c == 5 || c == 8) {
+						cout << "| ";
+					}
+				}
+				cout << endl;
+			}
+			cout << " ----------------------- " << endl;
+			/************************TEMP DEBUG CODE*****************************/
+
 			for (int y = 0; y < 9; y++) {
 				makeNum(sBoard, numVec, x);
 			}
@@ -43,15 +62,20 @@ public:
 		}
 	}
 
-	void makeNum(int arr[9][9], vector<int> arrV, int x) { //Generates a random horizontal vector of numbers 1-9
-		int randPos = 0 + (rand() % 8);
-		yPos = arrV[randPos];
-		arrV.erase(arrV.begin()+randPos);
-		arrV.shrink_to_fit();
-		cout << "x= " << x << endl;
-		cout << "yPos= " << yPos << endl;
+	void makeNum(int arr[9][9], vector<int> numV, int x) { //Generates a random horizontal vector of numbers 1-9
+		int randPos = 0 + (rand() % 8); //Finds a random position in the 1-9 list
+		yPos = numV[randPos];
+		//cout << "randPos= " << randPos; //Debug
+		numV.erase(numV.begin() + yPos); //Currently doesn't erase the value ???
+		numV.shrink_to_fit(); 
+		for (int i = 0; i <= numV.size(); i++) {
+			cout << numVec[i];
+		}
+		cout << endl;
+		cout << " x= " << x; //Debug
+		cout << " y= " << yPos << endl; //Debug
 		sBoard[x][yPos] = var;
-		cout << "sBoard= " << sBoard[x][yPos] << endl;
+		//cout << " sBoard= " << sBoard[x][yPos] << endl; //Debug
 		var--;
 	}
 	
@@ -60,14 +84,14 @@ private:
 	int var = 9;
 	int yPos;
 
-protected:
-	int diffIn;
+//protected:
+	//int diffIn;
 };
 
 class Sudoku : public Board {
 public:
 
-	void printBoard(int arr[9][9]) {
+	void printBoard(int arr[9][9]) { //prints the sudoku board with other aesthetics
 		cout << " ----------------------- " << endl;
 		for (int b = 0; b < 9; b++) {
 			if (b == 3 || b == 6 || b == 9) {
