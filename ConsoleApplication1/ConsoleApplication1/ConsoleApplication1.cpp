a1b41035208cff78e337c80049d5a18f165016b1
cgifford99 <cmgskiguy99@gmail.com>
2016-10-07 11:26:04 -0400

    Version 0.0.5b
    Organized and improved code. Currently crashes and throws "Vector
    subscript out of range". Going to debug tonight.

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,20 +5,18 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#include <algorithm>
+//#include <algorithm>
 using namespace std;
 
 class Board {
 public:
 	//int sizeX;
 	//int sizeY;
-	int sBoard[9][9];
 	//const enum diffInput { easy, medium, hard, };
 	
 	Board(){
-		for (int i = 0; i <= 8; i++) { //Adds values to the vector
-			numVec.push_back(i);
-		}
+		resetVector(numVec);
+		cout << "did thing" << endl;
 		/* Temp comment. Difficulty option currently unnecessary.
 		cout << "Enter difficulty: Easy, Medium or Hard?";
 		cin >> diffIn;
@@ -32,66 +30,40 @@ public:
 		case hard:
 			blanks = 54;
 		*/
+		genBoard(sBoard, numVec);
+		printBoard(sBoard);
 	}
 	
-	void genBoard() {
+	void genBoard(int arr[9][9], vector<int> numV) {
 		for (int x = 0; x < 9; x++) { //Sets values for the board
-
-			/************************TEMP DEBUG CODE*****************************/
-			cout << " ----------------------- " << endl;
-			for (int b = 0; b < 9; b++) {
-				if (b == 3 || b == 6 || b == 9) {
-					cout << "| ------+-------+--------" << endl;
-				}
-				cout << "| ";
-				for (int c = 0; c < 9; c++) {
-					cout << sBoard[b][c] << " ";
-					if (c == 2 || c == 5 || c == 8) {
-						cout << "| ";
-					}
-				}
-				cout << endl;
-			}
-			cout << " ----------------------- " << endl;
-			/************************TEMP DEBUG CODE*****************************/
-
+			printBoard(arr);
 			for (int y = 0; y < 9; y++) {
-				makeNum(sBoard, numVec, x);
+				makeNum(sBoard, numV, x);
 			}
 			var = 9;
 		}
 	}
 
 	void makeNum(int arr[9][9], vector<int> numV, int x) { //Generates a random horizontal vector of numbers 1-9
-		int randPos = 0 + (rand() % 8); //Finds a random position in the 1-9 list
-		yPos = numV[randPos];
-		//cout << "randPos= " << randPos; //Debug
-		numV.erase(numV.begin() + yPos); //Currently doesn't erase the value ???
-		numV.shrink_to_fit(); 
-		for (int i = 0; i <= numV.size(); i++) {
-			cout << numVec[i];
+		int randPos = 0 + (rand() % 9); //******MAGIC******//Finds a random position in the 1-9 list
+		resetVector(numV);
+		//yPos = numV[randPos];
+		cout << "randPos= " << randPos; //Debug
+		//numV.erase(numV.begin() + yPos); //Currently doesn't erase the value ???
+		numV.shrink_to_fit();
+		cout << numV.capacity() << endl;
+		for (int i = 0; i <= 1; i++) { //Prints the current vector
+			cout << numV[1];
 		}
 		cout << endl;
 		cout << " x= " << x; //Debug
 		cout << " y= " << yPos << endl; //Debug
-		sBoard[x][yPos] = var;
-		//cout << " sBoard= " << sBoard[x][yPos] << endl; //Debug
+		arr[x][yPos] = var;
+		cout << " sBoard= " << sBoard[x][yPos] << endl; //Debug
 		var--;
 	}
-	
-private:
-	vector<int> numVec;
-	int var = 9;
-	int yPos;
 
-//protected:
-	//int diffIn;
-};
-
-class Sudoku : public Board {
-public:
-
-	void printBoard(int arr[9][9]) { //prints the sudoku board with other aesthetics
+	void printBoard(int arr[9][9]) {
 		cout << " ----------------------- " << endl;
 		for (int b = 0; b < 9; b++) {
 			if (b == 3 || b == 6 || b == 9) {
@@ -108,11 +80,29 @@ public:
 		}
 		cout << " ----------------------- " << endl;
 	}
+
+	void resetVector(vector<int> vec) {
+		int arrOfTen[9] = { 0,1,2,3,4,5,6,7,8 };
+		vec.assign(arrOfTen, arrOfTen + 7);
+	}
+	
+private:
+	vector<int> numVec;
+	int var = 9;
+	int yPos;
+
+protected:
+	int sBoard[9][9];
+	//int diffIn;
 };
 
+/*
+class Sudoku : public Board {
+public:
+};
+*/
+
 int main() {
-	Sudoku gameObj; 
-	gameObj.genBoard();
-	gameObj.printBoard(gameObj.sBoard);
+	Board nBoard;
 	return 0;
 }
