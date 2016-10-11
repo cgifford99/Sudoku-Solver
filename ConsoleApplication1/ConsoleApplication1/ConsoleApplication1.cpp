1ed929bdf4d19f56871ca74f4a0636a81636d4a8
Christopher Gifford <cmgskiguy99@gmail.com>
2016-10-10 21:48:03 -0400

    Version 0.0.6
    Horizontal board generation works. Need to clean up code.

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -34,26 +34,26 @@ public:
 		printBoard(sBoard);
 	}
 	
-	void genBoard(int arr[9][9], vector<int> numV) {
+	void genBoard(int arr[9][9], vector<int> &numV) {
 		for (int x = 0; x < 9; x++) { //Sets values for the board
 			printBoard(arr);
 			for (int y = 0; y < 9; y++) {
 				makeNum(sBoard, numV, x);
 			}
+			resetVector(numV);
 			var = 9;
 		}
 	}
 
-	void makeNum(int arr[9][9], vector<int> numV, int x) { //Generates a random horizontal vector of numbers 1-9
-		int randPos = 0 + (rand() % 9); //******MAGIC******//Finds a random position in the 1-9 list
-		resetVector(numV);
-		//yPos = numV[randPos];
-		cout << "randPos= " << randPos; //Debug
-		//numV.erase(numV.begin() + yPos); //Currently doesn't erase the value ???
+	void makeNum(int arr[9][9], vector<int> &numV, int x) { //Generates a random horizontal vector of numbers 1-9
+		int randPos = 0 + (rand() % numV.size()); //******MAGIC******//Finds a random position in the 1-9 list
+		yPos = numV[randPos];
+		cout << "randPos= " << randPos << endl; //Debug
+		numV.erase(numV.begin() + randPos); //Currently doesn't erase the value ???
 		numV.shrink_to_fit();
-		cout << numV.capacity() << endl;
-		for (int i = 0; i <= 1; i++) { //Prints the current vector
-			cout << numV[1];
+		//cout << numV.capacity() << endl;
+		for (int i = 0; i < numV.size(); i++) { //Prints the current vector
+			cout << numV[i];
 		}
 		cout << endl;
 		cout << " x= " << x; //Debug
@@ -81,9 +81,16 @@ public:
 		cout << " ----------------------- " << endl;
 	}
 
-	void resetVector(vector<int> vec) {
+	void resetVector(vector<int> &vec) {
 		int arrOfTen[9] = { 0,1,2,3,4,5,6,7,8 };
-		vec.assign(arrOfTen, arrOfTen + 7);
+		vec.assign(arrOfTen, arrOfTen + 9);
+		printVector(vec, vec.size());
+	}
+	
+	void printVector(vector<int> &vec, int vecRange) {
+		for (int y = 0; y < vecRange; y++) {
+			cout << vec[y] << endl;
+		}
 	}
 	
 private:
